05c6965bcc38ec09982cf3e4c7565fe6aafe0cc1
Aline Silva <alinealv.silv@gmail.com>
2024-06-03 09:34:39 -0300

    alinhamento layout popup logout

--- employee-management-system/src/components/homescreen/popuplogout/popuplogoutstyles.tsx
+++ employee-management-system/src/components/homescreen/popuplogout/popuplogoutstyles.tsx
@@ -17,33 +17,59 @@ export const PopupContent = styled.div`
   padding: 20px;
   border-radius: 8px;
   width: 400px;
+  height:200px;
   max-width: 80%;
-  text-align: center;
+
 `;
 
 export const PopupHeader = styled.h2`
-  margin-bottom: 10px;
+margin-left:10%;
+font-family: 'Manrope', sans-serif;
+width:100%;
+justify-content: flex-start;
+font-size: 20px;
+margin-bottom: 32px;
 `;
 
 export const PopupBody = styled.p`
+margin-left:10%;
   margin-bottom: 20px;
 `;
 
 export const PopupFooter = styled.div`
   display: flex;
-  justify-content: space-between;
+  width:350px;
+  display: flex;
+  justify-content: flex-end;
+  align-items:flex-end;
+  margin-top:40px;
 `;
 
 export const Button = styled.button`
-  background-color: #ff4d4f;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
+width:85px;
+border: none;
+height:44px;
+border-radius: 5px;
+background-color: #ff5722;
+color: white;
+cursor: pointer;
+
+&:hover {
+  background-color: #e64a19;
+}
 `;
 
 export const ButtonCancel = styled(Button)`
-  background-color: #ccc;
-  color: black;
+width:85px;
+height:44px;
+margin-right:20px;
+border: none;
+background: none;
+color: orange;
+cursor: pointer;
+
+&:hover {
+  background-color: #eeee;
+  border-radius: 5px;
+}
 `;
